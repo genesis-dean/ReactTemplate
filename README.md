9ad8efa33fc75f1ce1f897bf5eb5cfc7267b974e
genesis-dean <dean.c@gen-game.com>
2021-05-31 17:32:53 +0800

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ReactTemplate
+
+This is a simple template integrating with:
+- React
+- Webpack
+- Typescript
+- ESLint
---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+	<h1>
+		React with Typescript Project!!
+	</h1>
+);
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById( 'root' )
+);
